1669c71de7ed2eb925f6c4e65884368fd79f5df9
idatsjm20242 <idat.daa2.sjm2024@gmail.com>
2024-08-15 21:10:48 -0500

    funciones de category service

--- src/app/dashboard/category/category.service.ts
+++ src/app/dashboard/category/category.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from './category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getAllCategory():Observable<Category[]>{
+    return this.httpClient.get<Category[]>("http://localhost:8080/category/listar")
+  }
+  getCategory(id: number):Observable<Category>{
+    return this.httpClient.get<Category>("http://localhost:8080/category/"+id)
+  }
+
+  createCategory(category: Category):Observable<Category>{
+    return this.httpClient.post<Category>("http://localhost:8080/category", category)
+  }
+
 }
