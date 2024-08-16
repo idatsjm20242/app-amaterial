3708493dc9062d464d597a128cbb72a510d1df17
idatsjm20242 <idat.daa2.sjm2024@gmail.com>
2024-08-15 21:00:13 -0500

    service category init

---
+++ src/app/dashboard/category/category.ts
@@ -0,0 +1,12 @@
+export interface Category{
+    /*
+    {
+        "categoryid": 9,
+        "categoryname": "Prueba",
+        "description": "prueba desde postman"
+    }
+     */
+    categoryid: number,
+    categoryname: string,
+    description: string
+}
